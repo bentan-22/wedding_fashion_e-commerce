b13ab172baef05e6e4c4c3bec2cfeddc50496fbd
methaveelu <110245885+methaveelu@users.noreply.github.com>
2023-05-02 14:48:37 +0800

    catogories_dropdown

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -3,10 +3,16 @@ import { Link } from 'react-router-dom';
 import styles from '../../styles/styles';
 import { categoriesData, productData } from "../../static/data";
 import { AiOutlineHeart,AiOutlineSearch, AiOutlineShoppingCart,} from "react-icons/ai";
+import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
+import {BiMenuAltLeft} from "react-icons/bi";
+import DropDown from "./DropDown";
+
 
 const Header = () => {
     const [searchTerm, setSearchTerm] =useState("")
     const [searchData, setSearchData] =useState(null)
+    const [active, setActive] = useState(false);
+    const [dropDown, setDropDown] = useState(false);
 
     const handleSearchChange = (e) => {
         const term = e.target.value;
@@ -22,6 +28,13 @@ const Header = () => {
         )
         setSearchData(filteredProducts);
     };
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 70) {
+          setActive(true);
+        } else {
+          setActive(false);
+        }
+    });
 
     return (
         <>
@@ -71,10 +84,45 @@ const Header = () => {
                   </div>
                 ) : null}
               </div>
-              <div>
-                
-              </div>
+              <div className={`${styles.button}`}>
+                 <Link to="/seller">  {/*{`${isSeller ? "/dashboard" : "/shop-create"}`}*/}
+                    <h1 className="text-[#fff] flex items-center">
+                    {/* {isSeller ? "Go Dashboard" : "Become Seller"}{" "} */}
+                    Become Seller <IoIosArrowForward className="ml-1" />
+                    </h1>
+                </Link>
+              </div>        
           
+            </div>
+
+            <div className = {`${
+          active === true ? "shadow-sm fixed top-0 left-0 z-10" : null
+        } transition hidden 800px:flex items-center justify-between w-full bg-[#3321c8] h-[70px]`}>
+                <div className={`${styles.section} relative ${styles.noramlFlex} justify-between`}>
+                    {/* categories */}
+                    <div onClick={() => setDropDown(!dropDown)}>
+                        <div className="relative h-[60px] mt-[10px] w-[270px] hidden 1000px:block">
+                            <BiMenuAltLeft size={30} className="absolute top-3 left-2" />
+                            <button
+                                className={`h-[100%] w-full flex justify-between items-center pl-10 bg-white font-sans text-lg font-[500] select-none rounded-t-md`}
+                            >
+                                All Categories
+                            </button>
+                            <IoIosArrowDown
+                                size={20}
+                                className="absolute right-2 top-4 cursor-pointer"
+                                onClick={() => setDropDown(!dropDown)}
+                            />
+                            {dropDown ? (
+                                <DropDown
+                                categoriesData={categoriesData}
+                                setDropDown={setDropDown}
+                                />
+                            ) : null}
+                        </div>
+                    </div>
+                        
+                </div>
             </div>
         </div>
         </>
