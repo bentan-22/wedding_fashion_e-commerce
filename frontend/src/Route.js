ce1dd0546e553e105e8867cfab907d7e5b4ae4a0
methaveelu <110245885+methaveelu@users.noreply.github.com>
2023-05-02 13:49:31 +0800

    searchbar with product filter

--- frontend/src/Route.js
+++ frontend/src/Route.js
@@ -1,8 +1,10 @@
 
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/SignupPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
 export {
     LoginPage,
     SignupPage,
+    HomePage,
 };
